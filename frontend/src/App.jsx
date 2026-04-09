8f676e63d2a39c4d7cff61af87d6e68993877bf9
JCBucio <juan.c4@hotmail.com>
2026-04-09 12:07:38 -0600

    Major web app update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import GlobeTracker from "./components/GlobeTracker";
+import Sidebar from "./components/Sidebar";
 import './index.css';
 
 function App() {
+  const [stations, setStations] = useState([]);
+  const [selectedStationName, setSelectedStationName] = useState('');
+
   return (
     <div className="app-container">
-      {/* Main component */}
-      <GlobeTracker />
+      <Sidebar
+        stations={stations}
+        selectedStationName={selectedStationName}
+        onStationSelect={setSelectedStationName}
+      />
+      <GlobeTracker
+        selectedStationName={selectedStationName}
+        onStationsUpdate={setStations}
+        onStationSelect={setSelectedStationName}
+      />
     </div>
   );
 }
