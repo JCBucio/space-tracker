6f1b5db836f8b329e10bae5d4295ce97c2eb67ee
JCBucio <juan.c4@hotmail.com>
2026-04-09 12:44:30 -0600

    Vite configuration update for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Le dice a Vite que apunte a navegadores modernos que soportan "top-level await"
+    target: 'esnext'
+  },
+  worker: {
+    // Fuerza a Vite a empaquetar los workers como módulos ES nativos en lugar de 'iife'
+    format: 'es'
+  }
+});
